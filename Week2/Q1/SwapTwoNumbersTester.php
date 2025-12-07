65049f9c43c655e73e169bafd3e78ea812d22bf2
Esmatullah-rah <esmatullah.rahnavard@gmail.com>
2025-12-07 21:31:40 +0430

    first question done and a Library for testing installed

--- Week2/Q1/SwapTwoNumbersTester.php
+++ Week2/Q1/SwapTwoNumbersTester.php
@@ -1,28 +1,21 @@
-/* Esmatullah
-1. Swap two variables without using a temporary variable
-Description: Write a program to swap the values of two variables (e.g., a and b) without using a
-third variable.
-Example:
-Input:
-a = 5, b = 10
-Output:
-a = 10, b = 5
-*/
 <?php
-class SwapTwoNumbers
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'SwapTwoNumbers.php';
+
+class SwapTwoNumbersTest extends TestCase
 {
-  function swap(int $a, int $b): void
+  public function testSwap()
   {
-    $a += $b;
-    $b = $a - $b;
-    $a = $a - $b;
-    echo "A is {$a} and B is {$b}";
+    $swap = new SwapTwoNumbers();
+
+    //$this->assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+    $this->assertEquals([5, 3], $swap->swap(3, 5));
+    $this->assertEquals([20, 10], $swap->swap(10, 20));
+    $this->assertEquals([2, 1], $swap->swap(1, 2));
+    $this->assertEquals([-52, 1002], $swap->swap(1002, -52));
+    $this->assertEquals([0, 512], $swap->swap(512, 0));
+    $this->assertEquals([-992, -85], $swap->swap(-85, -992));
   }
 }
-
-$obj = new SwapTwoNumbers;
-$obj->swap(5, 10);
-$obj->swap(-5, 7);
-$obj->swap(50, 18);
-$obj->swap(-9, 110);
-$obj->swap(45, 1);
