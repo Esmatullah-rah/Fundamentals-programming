05c339ed47b3171c88dc9a55f3918ce54e265570
Esmatullah-rah <esmatullah.rahnavard@gmail.com>
2025-12-07 19:46:06 +0430

    first changes

---
+++ Week2/Q!/Q1.php
@@ -0,0 +1,10 @@
+/* Esmatullah
+1. Swap two variables without using a temporary variable
+Description: Write a program to swap the values of two variables (e.g., a and b) without using a
+third variable.
+Example:
+Input:
+a = 5, b = 10
+Output:
+a = 10, b = 5
+*/
